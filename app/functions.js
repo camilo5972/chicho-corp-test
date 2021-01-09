defce8f73f0c12db4f05e18adcc9b0a18b2004c1
Camilo Henao Navarro <camilohenao@MBP-de-Camilo.lan>
2021-01-09 15:12:01 -0500

    chore: finish functions test

--- app/functions.js
+++ app/functions.js
@@ -2,38 +2,44 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.functionsAnswers = {
   argsAsArray: function(fn, arr) {
-
+    fn();
+    return `${arr[0]}, ${arr[1]}${arr[2]}`;
   },
 
   speak: function(fn, obj) {
-
+    return fn.call(obj);
   },
 
   functionFunction: function(str) {
-
+    return (str2) => `${str}, ${str2}`;
   },
 
   makeClosures: function(arr, fn) {
-
+    return arr.map(item => () => fn(item));
   },
 
   partial: function(fn, str1, str2) {
-
+    return (punctuation) => fn(str1, str2, punctuation);
   },
 
   useArguments: function() {
-
+    return [...arguments].reduce((previous, current) =>  current + previous, 0);
   },
 
   callIt: function(fn) {
-
+    const args = Array.prototype.slice.call(arguments, 1);
+    fn(...args);
   },
 
   partialUsingArguments: function(fn) {
-
+    const args = Array.prototype.slice.call(arguments, 1);
+    return function() {
+      var allArguments = args.concat(Array.prototype.slice.call(arguments));
+      return fn.apply(this, allArguments);
+    }
   },
 
   curryIt: function(fn) {
-
+    return (a) => (b) => (c) => fn(a, b, c);
   }
 };
