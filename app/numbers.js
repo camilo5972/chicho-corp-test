da3e22cc6982e55cfa726f8f36fe67b33d36bef6
Camilo Henao Navarro <camilohenao@MBP-de-Camilo.lan>
2021-01-09 16:00:48 -0500

    chore: finish numbers test

--- app/numbers.js
+++ app/numbers.js
@@ -2,15 +2,16 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.numbersAnswers = {
   valueAtBit: function(num, bit) {
-
+    const binary = [...num.toString(2)];
+    return bit > binary.length ? 0 : Number(binary[binary.length - bit]);
   },
 
   base10: function(str) {
-
+    return parseInt(str, 2)
   },
 
   convertToBinary: function(num) {
-
+    return num.toString(2).padStart(8, '0');
   },
 
   multiply: function(a, b) {
