15025baaa350d787fc4a362c96d993f203359095
Camilo Henao Navarro <camilohenao@MBP-de-Camilo.lan>
2021-01-09 15:39:51 -0500

    chore: finish module test

--- app/modules.js
+++ app/modules.js
@@ -1,7 +1,19 @@
 exports = typeof window === 'undefined' ? global : window;
 
+class Module {
+  name;
+  greeting;
+
+  constructor(name, greeting) {
+    this.name = name;
+    this.greeting = greeting;
+  }
+
+  sayIt = () => `${this.greeting}, ${this.name}`;
+}
+
 exports.modulesAnswers = {
   createModule: function(str1, str2) {
-
+    return new Module(str2, str1);
   }
 };
