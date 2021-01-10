603f79e9051a51101ef9c8fc448b8b89a415698d
Camilo Henao Navarro <camilohenao@MBP-de-Camilo.lan>
2021-01-10 12:20:54 -0500

    chore: finish regex test

--- app/regex.js
+++ app/regex.js
@@ -2,26 +2,33 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.regexAnswers = {
   containsNumber: function(str) {
-
+    const regex = new RegExp(/\d/);
+    return regex.test(str);
   },
-
+  
   containsRepeatingLetter: function(str) {
-
+    const regex = new RegExp(/([a-z])\1/i);
+    return regex.test(str);
   },
-
+  
   endsWithVowel: function(str) {
-
+    const regex = new RegExp(/(a|e|i|o|u)$/i);
+    return regex.test(str);
   },
-
+  
   captureThreeNumbers: function(str) {
-
+    const regex = new RegExp(/\d{3}/);
+    if (!regex.test(str)) return false;
+    return regex.exec(str).join('');
   },
-
+  
   matchesPattern: function(str) {
-
+    const regex = new RegExp(/^\d{3}-\d{3}-\d{4}$/);
+    return regex.test(str);
   },
-
+  
   isUSD: function(str) {
-
+    const regex = new RegExp(/^\$(\d+|\d{1,3}(,\d{3}){2})(\.\d+)?$/);
+    return regex.test(str);
   }
 };
