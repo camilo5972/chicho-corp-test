6e186b544c8d387af7068a62b6f5118afdd99f01
Camilo Henao Navarro <camilohenao@MBP-de-Camilo.lan>
2021-01-10 17:30:38 -0500

    chore: finis strings test

--- app/strings.js
+++ app/strings.js
@@ -2,14 +2,17 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.stringsAnswers = {
   reduceString: function(str, amount) {
-
+    const regex = new RegExp(`(.)(?=\\1{${amount},})`, 'g');
+    return str.replace(regex, '');
   },
 
   wordWrap: function(str, cols) {
-
+    const regex = new RegExp(/(^|[\s])[^\s]{2,}([\s]|$)/, 'g');
+    const replacer = (value) => value.replaceAll(' ', '\n');
+    return str.replace(regex, replacer);
   },
 
   reverseString: function(str) {
-
+    return str.split('').reverse().join('');
   }
 };
