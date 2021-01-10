33388c0b9b6548fcc8a11016f6cb58b04ad6a592
Camilo Henao Navarro <camilohenao@MBP-de-Camilo.lan>
2021-01-10 17:30:52 -0500

    chore: finish counter test

--- app/count.js
+++ app/count.js
@@ -2,6 +2,17 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.countAnswers = {
   count: function (start, end) {
-
+    let cont = start;
+    console.log(start);
+    cont++;
+    const interval = setInterval(() => {
+      if (cont === end) clearInterval(interval);
+      console.log(cont);
+      cont++;
+    }, 100);
+    const clear = () => {
+      clearInterval(interval);
+    }
+    return { cancel: clear}
   }
 };
